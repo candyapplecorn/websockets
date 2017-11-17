d91fb81fe506d21fe15fc2f6dd82f47ac950be34
App Academy Student <student@appacademy.io>
2017-11-17 11:32:55 -0800

    added page index and root to page index routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'page/index'
+	resource :page, only: [:index]
+
+	root 'page#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
